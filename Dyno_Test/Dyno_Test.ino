2391f9e799ed031d5bebd8fe1871129e98c7fac9
Reegan McAllister <reegan@ualberta.ca>
2016-02-27 11:51:05 -0700

    Faster interrupts, only one encoder

--- Dyno_Test/Dyno_Test.ino
+++ Dyno_Test/Dyno_Test.ino
@@ -2,53 +2,69 @@
 
 //compiler directives to speed up interrupts and save ram
 //#define EI_NOTEXRERNAL  //uncomment if you are not using external interrupts
-//#define EI_NOTINT0 //or this if you are only using int1
+#define EI_NOTINT0 //or this if you are only using int1
 //#define EI_NOTINT1 //or this if you are only using int0
-//#define EI_NOTPINCHANGE //uncomment if you are using pin change interrupts
-//#define EI_NOTPORTB // uncomment if no port B interrupts are used
-//#define EI_NOTPORTC // same for port C
+#define EI_NOTPINCHANGE //uncomment if you are using pin change interrupts
+#define EI_NOTPORTB // uncomment if no port B interrupts are used
+#define EI_NOTPORTC // same for port C
 //#define EI_NOTPORTD // port D
 
-#include <EnableInterrupt.h>
+#define NEEDFORSPEED
+#define INTERRUPT_FLAG_PIN3 pulse_count
 
-//define the hall effect sensor pins
-#define hall_1 1
-#define hall_2 2
-#define hall_3 3
+#include <EnableInterrupt.h>
+#include <Servo.h>
+
+//define the hall effect sensor pin
+#define hall_pin 3 //int1
 
 //define curent input pin
 #define current_pin A0
 
+//define esc pwm pin
+#define servo_pin 10
+
 //make variables for storing data
-uint16_t pulse_count = 0;
 uint16_t last_pulse_count = 0;
 uint16_t motor_current = 0;
 
 //make variable for timing writing to serial port
 uint32_t time_var = 0;
 
-//make function for counting pulses
-void count_pulses (void)
-{
-  pulse_count++;
-}
+Servo esc; //create servo object
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(hall_1,INPUT);
-  pinMode(hall_2,INPUT);
-  pinMode(hall_3,INPUT);
+  pinMode(hall_pin,INPUT);
 
   pinMode(current_pin,INPUT);
 
-  //set up interrupts
-  enableInterrupt(hall_1,count_pulses,RISING); //every time hall_1 pin rises count_pulses function wil run
-  enableInterrupt(hall_2,count_pulses,RISING);
-  enableInterrupt(hall_3,count_pulses,RISING);
+  enableInterruptFast(hall_pin, RISING);
 
+  esc.attach(servo_pin);
+  //ESC needs a low high low signal to turn on
+  esc.write(0);
+  delay(1000);
+  esc.write(180);
+  delay(500);
+  esc.write(0);
+  delay(500);
+
+  //give person 10 seconds to run away
+  delay(10000);
+
+  //datalogger starts when a $ is sent
+  Serial.print("$");
+
+  //reset pulse count
+  pulse_count = 0;
+  
   //reset time
   time_var = millis();
+
+  //start motor
+  esc.write(180);
 }
 
 
