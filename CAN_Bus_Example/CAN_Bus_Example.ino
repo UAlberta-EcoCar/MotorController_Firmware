d186287181f1f00a083e22797c1dd2c6cbed5513
Reegan McAllister <reegan@ualberta.ca>
2016-03-15 15:54:33 -0600

    Read Brake Pedal Values

--- CAN_Bus_Example/CAN_Bus_Example.ino
+++ CAN_Bus_Example/CAN_Bus_Example.ino
@@ -1,17 +1,14 @@
 //this sketch simulates a node in the CAN network
 
 #include <mcp2515_lib.h>
+#include <mcp2515_filters.h>
 #include <can_message.h>
 #include <can_message_def.h>
 
-/*
- * - This node will be sending:
- *      - MESSAGE_FC_LOGGING_FCTEMP_ID twice a second
- *      - MESSAGE_FC_LOGGING_FCVOLT_FCCURR_ID four times a second
- *      - MESSAGE_FC_LOGGING_CAPVOLT_CAPCURR_ID 5 times a second
- */
-
+//Create throttle_msg
 can_msg::MsgEncode throttle_msg( can_msg::UINT16, can_msg::MOTOR, can_msg::THROTTLE, can_msg::CRITICAL, 1 );
+//Create brake_msg
+can_msg::MsgEncode brake_msg( can_msg::UINT16, can_msg::MOTOR, can_msg::BRAKE, can_msg::CRITICAL, 1 );
 
 unsigned char var = 0;
 #define NL Serial.println();
@@ -24,7 +21,7 @@ void setup() {
 
  // Initialize CAN
  Serial.print("Initializing mcp2515 CAN controller... ");
-  if (can_init(0,0,0,0,0,0,0,0)){ //filter set to accept all messages
+  if (can_init(0,0,0,0,0,0,0,0)){ //filter set to accept throttle and brake msg's
     Serial.println("mcp2515 config error");
     while(1); //hang up program
   }
@@ -43,10 +40,16 @@ void loop()
   if(digitalRead(9) == 0) //If there was a "message received interrupt" (happy mike?)
   {
     message = can_get_message(); //retrieve message
-    if(message.id == throttle_msg.id()) //if message is a throttle message
+    if(message.id == throttle_msg.id()) //if message is a throttle message (the "&(DEVICE_MASK|MESSAGE..)" makes code only check message and device parts of the ID
     {
       int throttle = message.data[0] | (message.data[1] << 8); //read data
       Serial.println(throttle); //display data
+    }
+    else if((message.id&(DEVICE_MASK|MESSAGE_MASK)) == (brake_msg.id()&(DEVICE_MASK|MESSAGE_MASK))) //if message is a throttle message (the "&(DEVICE_MASK|MESSAGE..)" makes code only check message and device parts of the ID
+    {
+      int brake = message.data[0] | (message.data[1] << 8); //read data
+      Serial.print("     ");
+      Serial.println(brake); //display data
     }
     
   }
